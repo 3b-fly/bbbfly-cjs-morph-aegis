d7a02066a6bd4dcb5b975d1899bba62b1e3ccade
3b-fly <member@3b-fly.eu>
2022-07-30 16:26:06 +0200

    Added "style-map" package.

---
+++ build/release/style/map/morph-style.js
@@ -0,0 +1,8 @@
+/*!
+ * @author Jan Nejedly support@3b-fly.eu
+ * @copyright Jan Nejedly
+ * @version 1.0.1
+ * @license see license in 'LICENSE' file
+*/
+
+bbbfly.Morph&&bbbfly.Morph.RegisterStyle({ID:"morph-aegis-map",Lib:"bbbfly-cjs-morph-aegis",ImgDir:"style/map/imgs/",Prefix:"aegis-map-",Sources:null,Images:null,OnInit:function(){this.Sources=bbbfly.morph.style.map.Images.Sources();this.Images=bbbfly.morph.style.map.Images.Images()}});
---
+++ src/style/map/morph-style.js
@@ -0,0 +1,24 @@
+﻿/**
+ * @file
+ * @author Jan Nejedly [support@3b-fly.eu]
+ * @copyright Jan Nejedly
+ *
+ * @inpackage style-map
+ */
+
+if(bbbfly.Morph){
+  bbbfly.Morph.RegisterStyle({
+    ID: 'morph-aegis-map',
+    Lib: 'bbbfly-cjs-morph-aegis',
+    ImgDir: 'style/map/imgs/',
+    Prefix: 'aegis-map-',
+
+    Sources: null,
+    Images: null,
+
+    OnInit: function(){
+      this.Sources = bbbfly.morph.style.map.Images.Sources();
+      this.Images = bbbfly.morph.style.map.Images.Images();
+    }
+  });
+}
